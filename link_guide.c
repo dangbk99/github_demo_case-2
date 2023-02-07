09fc2ac75e1b2ac68d86e4bd993335d280aff2c9
unknown <dinhhaidang21121999@gmail.com>
2023-02-07 23:57:40 +0700

    up

--- link_guide.c
+++ link_guide.c
@@ -1,1 +1,2 @@
-https://www.notion.so/B-i-1-H-ng-d-n-s-d-ng-GIT-efbc4f5e9b404b94bb8ba4914058e475
+Link: https://www.notion.so/B-i-1-H-ng-d-n-s-d-ng-GIT-efbc4f5e9b404b94bb8ba4914058e475
+link git hub demo: https://github.com/Qspace/Embedded-Linux-Training
